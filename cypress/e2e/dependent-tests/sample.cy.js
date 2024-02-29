c6aa8390742ec009b1508eb12942e8113f3686ca
cinthiabs <cinthiabarbosa3d@gmail.com>
2024-02-28 21:54:21 -0300

    curso

--- cypress/e2e/dependent-tests/sample.cy.js
+++ cypress/e2e/dependent-tests/sample.cy.js
@@ -8,31 +8,29 @@ describe('Dependent tests bad practice', () => {
     cy.get('#password').type(Cypress.env('user_password'), { log: false })
     cy.get('button[type="submit"]').click()
     
-    cy.contains('h1', 'Your Notes').should('be.visible')
+    cy.contains('h1', 'Your Notes',{timeout:10000}).should('be.visible')
   })
+ it('CRUD a note',() => {
+  cy.contains('Create a new note').click()
 
-  it('creates a note', () => {
-    cy.contains('Create a new note').click()
+  cy.get('#content').type('My note')
+  cy.contains('Create').click()
+  
+  //Assert the note was created
+  cy.get('.list-group').should('contain', 'My note').click()
 
-    cy.get('#content').type('My note')
-    cy.contains('Create').click()
+  //updates the note
+  cy.get('#content').type(' updated')
+  cy.contains('Save').click()
 
-    cy.get('.list-group').should('contain', 'My note')
-  })
+  //assert the note was update
+  cy.get('.list-group').should('contain', 'My note updated')
+  cy.get('.list-group:contains(My note updated)').should('be.visible').click()
 
-  it('edits a note', () => {
-    cy.get('.list-group').contains('My note').click()
-    cy.get('#content').type(' updated')
-    cy.contains('Save').click()
+  //delete
+  cy.contains('Delete').click()
+ //assert the note was deleted
+  cy.get('.list-group:contains(My note updated)').should('not.exist')
+ })
 
-    cy.get('.list-group').should('contain', 'My note updated')
-    cy.get('.list-group:contains(My note updated)').should('be.visible')
-  })
-
-  it('deletes a note', () => {
-    cy.get('.list-group').contains('My note updated').click()
-    cy.contains('Delete').click()
-
-    cy.get('.list-group:contains(My note updated)').should('not.exist')
-  })
 })
